826da0ef03cef8d06005e5a604db8ff07e6e2bf0
Ronald Perdigão <git.ronaldseabra@gmail.com>
2023-02-09 21:04:50 -0300

    mudanças básicas pra aceitar requisiçoes ajax

--- spider-web/spider_web/settings.py
+++ spider-web/spider_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=-$$x+h*35k#)-f&9$wg83938r&x$fmebuwf5ekav9l@w&tqet
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,12 +37,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external apps
     "rest_framework",
+    "corsheaders",
     # my apps
     "core"
 ]
 
 MIDDLEWARE = [
+    # external middlewares
+    "corsheaders.middleware.CorsMiddleware",
+    # native middlewares
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -50,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
 ]
 
 ROOT_URLCONF = "spider_web.urls"
@@ -124,3 +130,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# requests config
+CSRF_COOKIE_NAME = 'XSRF-TOKEN'
+CSRF_HEADER_NAME = 'HTTP_X_XSRF_TOKEN'
+
+# External apps config
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  "http://127.0.0.1:3001","http://localhost:3001"
+)
