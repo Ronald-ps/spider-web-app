ecfbc4346dba93c304d783f250b513a40aee7085
Ronald Perdigão <git.ronaldseabra@gmail.com>
2023-02-13 14:15:34 -0300

    deleta a pasta atual de front, vou fazer com Vite

--- spider-web-front/apis/spider_web/config.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8001",
-  xsrfHeaderName: 'X-XSRF-TOKEN',
-  xsrfCookieName: 'XSRF-TOKEN',
-})
-
-export default api
--- spider-web-front/apis/spider_web/teste.js
+++
@@ -1,7 +0,0 @@
-import api from './config'
-
-export default {
-  get() {
-    api.get("/").then(r => r.data)
-  }
-}
