76a499fbc4008ddc43088df8f16c6fe8a4d3c32a
Ronald Perdigão <git.ronaldseabra@gmail.com>
2023-02-20 18:20:57 -0300

    modelo de documento externo criado

--- spider-web/spider_web/urls.py
+++ spider-web/spider_web/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
+# fmt: off
+urlpatterns = [
+                path("admin/", admin.site.urls),
+                path("", include("core.urls"))
+              ]
+# fmt: on
