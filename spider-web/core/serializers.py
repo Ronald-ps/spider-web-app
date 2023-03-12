e17b3b80990d3999288e339194c8d915175b94b0
Ronald Perdigão <git.ronaldseabra@gmail.com>
2023-03-11 21:55:52 -0300

    melhora comentários

--- spider-web/core/serializers.py
+++ spider-web/core/serializers.py
@@ -4,6 +4,8 @@ from core.models import Document
 
 
 class DocumentSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer básico da classe Document (documentos 'internos')"""
+
     class Meta:
         model = Document
         fields = ["name", "path", "inserted_at"]
