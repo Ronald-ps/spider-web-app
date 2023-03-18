ab6cc245eece7821b21c60d34d559685e4045e5c
Ronald Perdigão <git.ronaldseabra@gmail.com>
2023-03-18 19:53:56 -0300

    Add campo description_search_vector

--- spider-web/core/models.py
+++ spider-web/core/models.py
@@ -1,3 +1,5 @@
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVector, SearchVectorField
 from django.db import models
 
 NO_EMPTY = {"null": False, "blank": False}
@@ -17,9 +19,26 @@ class Document(models.Model):
     """Documento criado por esse projeto"""
 
     name = models.TextField(**NO_EMPTY)
+    # Nome do document
     path = models.TextField(**NO_EMPTY)
+    # Caminho do documento, como um caminho de arquivo no linux
     description = models.TextField(null=True, blank=True)
+    # Descrição do documento
+    description_search_vector = SearchVectorField(null=True)
+    # Descrição traduzida para um search vector, para que seja fácil fazer pesquisa full text
     inserted_at = models.DateTimeField(auto_now_add=True, **NO_EMPTY)
+    # Data em que o documento é inserido no banco de dados
+
+    def update_description_search_vector(self):
+        """Método utilizado para atualizar o description search vector,
+        implementado afim de que não seja necessário atualizar o search vector em runtime"""
+
+        vector = SearchVector("description")
+        self.description_search_vector = vector
+        self.save(update_fields=["description_search_vector"])
+
+    class Meta:
+        indexes = [GinIndex(fields=["description_search_vector"])]
 
 
 class ExternalDocument(models.Model):
